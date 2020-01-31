01878f81c38beb1b7c1176b49c973fc23a051df3
Lucas Emery <lemery@bottomline.com>
2020-01-31 07:11:17 -0500

    Deploy burrow default templates

--- manifests/config.pp
+++ manifests/config.pp
@@ -19,4 +19,23 @@ class burrow::config (
     content => epp('burrow/config.pp', $_config),
   }
 
+  $config_file_dir = dirname($config_file)
+
+  $_default_templates = [
+    'default-email.tmpl',
+    'default-http-delete.tmpl',
+    'default-http-post.tmpl',
+    'default-slack-delete.tmpl',
+    'default-slack-post.tmpl',
+  ]
+
+  $_default_templates.each |$template| {
+    file { "${config_file_dir}/${template}":
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      source => "puppet:///modules/burrow/${template}",
+    }
+  }
+
 }
