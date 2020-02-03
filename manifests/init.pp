e500e327ccc61b86a5746e92ea7064c9cf40dc19
Lucas Emery <lemery@bottomline.com>
2020-02-03 04:33:20 -0500

    Bunch of yak shaving to get config params to config template

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,202 +2,200 @@
 #
 # A description of what this class does
 #
-# @example
+# @param example
 #   include burrow
 #
-# @version
+# @param version
 #   Version of the burrow package to install.  Defaults to 1.3.2.
 #
-# @config_file
+# @param config_file
 #   Path and name for burrow config file.  Defaults to '/etc/burrow/config'.
 #
-# @pidfile
+# @param pidfile
 #   The path and filename to store the PID of the running process in. Defaults to '/var/run/burrow.pid'.
 #
-# @stdout_logfile
+# @param stdout_logfile
 #   If specified, it is the path and filename to redirect stdout and stderr into. If not specified, stdout/stderr are not redirected.  Defaults to ''.
 #
-# @access_control_allow_origin
+# @param access_control_allow_origin
 #   The value to put in the Access-Control-Allow-Origin header in responses from the HTTP server in Burrow. Defaults to ''.
 #
-# @logging_file
+# @param logging_file
 #   The path and filename to write logs to. If not specified, logs are written to stdout. Defaults to ''.
 #
-# @logging_level
+# @param logging_level
 #   	The minimum log level to output. Must be one of "debug", "info", "warn", "error", "panic", "fatal".  Defaults to 'info'.
 #
-# @logging_maxsize
+# @param logging_maxsize
 #   The maximum size for a log file, in MB. Defaults to 100.
 #
-# @logging_maxbackups
+# @param logging_maxbackups
 #   The maximum number of log files to maintain. Defaults to 10.
 #
-# @logging_maxage
+# @param logging_maxage
 #   The maximum number of days to keep log files for. Defaults to 30.
 #
-# @logging_use_localtime
+# @param logging_use_localtime
 #   If true, write log entries using the system's local time. Otherwise, use UTC.  Defaults to false.
 #
-# @logging_use_compression
+# @param logging_use_compression
 #   If true, compress rotated log files. Defaults to false.
 #
-# @zookeeper_servers
+# @param zookeeper_servers
 #   An array of strings in the form "hostname:port" that point to the servers in the Zookeeper ensemble. At least one is required.
 #
-# @zookeeper_timeout
+# @param zookeeper_timeout
 #   The expiration timeout for Zookeeper sessions, in seconds. Defaults to 6.
 #
-# @zookeeper_root_path
+# @param zookeeper_root_path
 #   A full path to the znode that Burrow is allow to write under. The path will be created if it does not already exist.  Defaults to '/burrow/notifier'.
 #
-# @manage_client_profile
+# @param manage_client_profile
 #   Whether to use this module to manage a client profile.  Defaults to false.
 #
-# @client_subheading
+# @param client_subheading
 #   Unique subheading name for client profile.  Defaults to 'myclient'.
 #
-# @client_kafka_version
+# @param client_kafka_version
 #   The client version of Kafka to support.
 #   Valid versions are: 0.8, 0.8.0, 0.8.1, 0.8.2, 0.9, 0.9.0, 0.9.0.0, 0.9.0.1, 0.10, 0.10.0, 0.10.0.0, 0.10.0.1, 0.10.1, 0.10.1.0, 0.10.2, 0.10.2.0, 0.11.0,
 #   0.11.0.1, 0.11.0.2, 1.0.0, 1.0.1, 1.0.2, 1.1.0, 1.1.1.  Defaults to 0.8.
 #
-# @client_id
+# @param client_id
 #   A string to be passed to Kafka as the client ID.  Defaults to 'burrow-lagchecker'.
 #
-# @client_tls
+# @param client_tls
 #   If specified, it is the name of a TLS profile to use in this client configuration.  Defaults to ''.
 #
-# @client_sasl
+# @param client_sasl
 #   If specified, it is the name of a SASL profile to use in this client configuration.  Defaults to ''.
 #
-# @manage_tls_profile
+# @param manage_tls_profile
 #   Whether to use this module to manage a TLS profile.  Defaults to false.
 #
-# @tls_subheading
+# @param tls_subheading
 #   Unique subheading name for TLS profile.  Defaults to 'mytlsprofile'.
 #
-# @tls_certfile
+# @param tls_certfile
 #   The path to the certificate file to be used (either a client certificate for client connections, or a server certificate for the HTTP server).
 #   Required if using this module to manage a TLS profile.
 #
-# @tls_keyfile
+# @param tls_keyfile
 #   The path to the key file that matches the certfile.  Defaults to ''.  Required if using this module to manage a TLS profile.
 #
-# @tls_cafile
+# @param tls_cafile
 #   If specified, it is the path to a bundle of certificates that provide the trusted root certificates for the connection.  Defaults to ''.
 #
-# @tls_no_verify
+# @param tls_no_verify
 #   	If true, do not validate the trust of the certificate sent by the other side of this connection.  Defaults to false.
-# @manage_sasl_profile
+# @param manage_sasl_profile
 #   Whether to use this module to manage a TLS profile.  Defaults to false.
 #
-# @sasl_subheading
+# @param sasl_subheading
 #   Unique subheading name for TLS profile.  Defaults to 'mysaslprofile'.
 #
-# @sasl_username
+# @param sasl_username
 #   The username to use for authentication. Required if using this module to manage a SASL profile.
 #
-# @sasl_password
+# @param sasl_password
 #   The password to use for authentication. Required if using this module to manage a SASL profile.
 #
-# @sasl_handshake_first
+# @param sasl_handshake_first
 #   If false, do not send SASL handshake first (only used for Sarama connections to Kafka when using a non-Kafka proxy).  Defaults to true.
 #
-# @manage_httpserver_profile
+# @param manage_httpserver_profile
 #   Whether to use this module to configure the HTTP server in Burrow.  Defaults to false.
 #
-# @httpserver_subheading
+# @param httpserver_subheading
 #   Unique subheading name for HTTP server.  Defaults to 'mylistener'.
 #
-# @httpserver_address
+# @param httpserver_address
 #   This is a string of the form ip:port that specifies what the server should listen on. If the ip part is left blank, the listener attaches to
 #   all IPs on the host. If the port part is left blank, a random port is chosen.  Required if using this module to manage the HTTP server in Burrow.
 #
-# @httpserver_timeout
+# @param httpserver_timeout
 #   The timeout and keepalive setting for connections, in seconds.  Defaults to 300.
 #
-# @httpserver_tls
+# @param httpserver_tls
 #   If specified, it must be the name of a configured TLS profile and the listener is started with this configuration. Defaults to ''.
 #
-# @manage_storage_profile
+# @param manage_storage_profile
 #   Whether to use this module to manage a storage profile.  Defaults to false.
 #
-# @storage_subheading
+# @param storage_subheading
 #   Unique subheading name for storage profile.  Defaults to 'mystorage'.
 #
-# @storage_class_name
+# @param storage_class_name
 #   This is the name of the module type. The only permitted value is "inmemory".
 #
-# @storage_intervals
+# @param storage_intervals
 #   The number of offsets to store for each partition.  Defaults to 10.
 #
-# @storage_expire_group
+# @param storage_expire_group
 #   The number of seconds after which a group will be purged if it has not committed offsets.  Defaults to 604800.
 #
-# @storage_workers
+# @param storage_workers
 #   The number of worker goroutines to start up. This number can be increased to provide more parallelism in the storage module.  Defaults to 20.
 #
-# @storage_min_distance
+# @param storage_min_distance
 #   Coalesce offset commits that are more frequent than this number of seconds. Used with the intervals configuration, this can be used to set a minimum
 #   time for the offset window (10 offsets x 15s = 150s minimum window).  Defaults to 0.
 #
-# @storage_group_whitelist
+# @param storage_group_whitelist
 #   If specified, ignore data for groups that do not match the regular expression.  Defaults to ''.
 #
-# @manage_evaluator_profile
+# @param manage_evaluator_profile
 #   Whether to use this module to configure the evaluator subsystem.  Defaults to false.
 #
-# @evaluator_subheading
+# @param evaluator_subheading
 #   Unique subheading name for evaluator subsystem.  Defaults to 'myeval'.
 #
-# @evaluator_class_name
+# @param evaluator_class_name
 #   This is the name of the module type. The only permitted value is "caching".
 #
-# @evaluator_expire_cache
+# @param evaluator_expire_cache
 #   The number of seconds to cache evaluation results for.  Defaults to 604800.
 #
-# @evaluator_minimum_complete
+# @param evaluator_minimum_complete
 #   A minimum threshold for completeness for evaluating partitions: any partition which has a completeness below this value will be marked as OK without evaluating it.
 #   Defaults to '0.0'
 #
-# @manage_cluster_profile
+# @param manage_cluster_profile
 #   It is not required to specify a cluster profile, but if you don't you won't have Burrow doing any work.  Defaults to true.
 #
-# @cluster_subheading
+# @param cluster_subheading
 #   Unique subheading name for cluster profile.  Defaults to 'mycluster'.
 #
-# @cluster_class_name
+# @param cluster_class_name
 #   Name of cluster module. The only permitted value is 'kafka'.
 #
-# @cluster_servers
+# @param cluster_servers
 #   An array of strings in the form "hostname:port" that point to the servers in the Kafka cluster. At least one is required.
 #
-# @cluster_client_profile
+# @param cluster_client_profile
 #   The name of a configured client-profile section. If no profile is specified, a default configuration for the client will be used. Defaults to ''.
 #
-# @cluster_topic_refresh
+# @param cluster_topic_refresh
 #   How often to refresh the list of all topics in the cluster, in seconds. Defaults to 60.
 #
-# @cluster_offset_refresh
+# @param cluster_offset_refresh
 #   How often to refresh the broker offset for each partition, in seconds. Defaults to 10.
 #
-# @manage_consumers
+# @param manage_consumers
 #   It is not required to specify this section, but if you don't you won't have Burrow doing much work. Defaults to true.
 #
-# @consumers
+# @param consumers
 #   An array of consumers.
 #
-# @manage_notifiers
+# @param manage_notifiers
 #   Defaults to false.
 #
-# @notifiers
+# @param notifiers
 #   An array of notifiers.
 #
 class burrow (
   String $version,
   String $config_file,
-  Array[String] $consumer_class_name,
-  Array[String] $notifiers_class_name,
   Data $pidfile,
   Data $stdout_logfile,
   String $access_control_allow_origin,
@@ -313,8 +311,68 @@ class burrow (
   }
 
   class { 'burrow::config':
-    config_file => $config_file,
+    config_file                 => $config_file,
+    pidfile                     => $pidfile,
+    stdout_logfile              => $stdout_logfile,
+    access_control_allow_origin => $access_control_allow_origin,
+    logging_filename            => $logging_filename,
+    logging_level               => $logging_level,
+    logging_maxsize             => $logging_maxsize,
+    logging_maxbackups          => $logging_maxbackups,
+    logging_maxage              => $logging_maxage,
+    logging_use_localtime       => $logging_use_localtime,
+    logging_use_compression     => $logging_use_compression,
+    zookeeper_servers           => $zookeeper_servers,
+    zookeeper_timeout           => $zookeeper_timeout,
+    zookeeper_root_path         => $zookeeper_root_path,
+    manage_client_profile       => $manage_client_profile,
+    client_subheading           => $client_subheading,
+    client_kafka_version        => $client_kafka_version,
+    client_id                   => $client_id,
+    client_tls                  => $client_tls,
+    client_sasl                 => $client_sasl,
+    manage_tls_profile          => $manage_tls_profile,
+    tls_subheading              => $tls_subheading,
+    tls_certfile                => $tls_certfile,
+    tls_keyfile                 => $tls_keyfile,
+    tls_cafile                  => $tls_cafile,
+    tls_no_verify               => $tls_no_verify,
+    manage_sasl_profile         => $manage_sasl_profile,
+    sasl_subheading             => $sasl_subheading,
+    sasl_username               => $sasl_username,
+    sasl_password               => $sasl_password,
+    sasl_handshake_first        => $sasl_handshake_first,
+    manage_httpserver_profile   => $manage_httpserver_profile,
+    httpserver_subheading       => $httpserver_subheading,
+    httpserver_address          => $httpserver_address,
+    httpserver_timeout          => $httpserver_timeout,
+    httpserver_tls              => $httpserver_tls,
+    manage_storage_profile      => $manage_storage_profile,
+    storage_subheading          => $storage_subheading,
+    storage_class_name          => $storage_class_name,
+    storage_intervals           => $storage_intervals,
+    storage_expire_group        => $storage_expire_group,
+    storage_workers             => $storage_workers,
+    storage_min_distance        => $storage_min_distance,
+    storage_group_whitelist     => $storage_group_whitelist,
+    manage_evaluator_profile    => $manage_evaluator_profile,
+    evaluator_subheading        => $evaluator_subheading,
+    evaluator_class_name        => $evaluator_class_name,
+    evaluator_expire_cache      => $evaluator_expire_cache,
+    evaluator_minimum_complete  => $evaluator_minimum_complete,
+    manage_cluster_profile      => $manage_cluster_profile,
+    cluster_subheading          => $cluster_subheading,
+    cluster_class_name          => $cluster_class_name,
+    cluster_servers             => $cluster_servers,
+    cluster_client_profile      => $cluster_client_profile,
+    cluster_topic_refresh       => $cluster_topic_refresh,
+    cluster_offset_refresh      => $cluster_offset_refresh,
+    manage_consumers            => $manage_consumers,
+    consumers                   => $consumers,
+    manage_notifiers            => $manage_notifiers,
+    notifiers                   => $notifiers,
   }
+  contain 'burrow::config'
 
   class { 'burrow::service':
   }
